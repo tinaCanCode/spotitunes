369f1db851262f93bf539467b8bc107fa1b3c8c0
tinaCanCode <mc.scherzberg@googlemail.com>
2020-11-25 17:48:50 +0100

    Podcasts can be added to user model

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Podcast = require('../models/Podcast');
+const User = require('../models/User');
 const router  = express.Router();
 
 /* GET home page */
@@ -18,9 +19,16 @@ router.post('/add-favorite', (req, res, next) => {
   console.log(req.body.spotifyid)
   Podcast.create({podcastId: req.body.spotifyid})
   // Add ObjectId of newly created Podcast to Users favorite podcasts
-  .then(resp => console.log("Response: " + resp.id))
+  .then(resp => {
+    console.log("Response: " + resp.id);
+    console.log("currentUser", req.session.currentUser._id)
+    console.log("favoritePodcasts", req.session.currentUser.favoritePodcasts)
+    //let updatedPodcasts = req.session.currentUser.favoritePodcasts.push(resp.id)
+    return User.findOneAndUpdate({_id: req.session.currentUser._id}, { $push: { favoritePodcasts: resp.id } }, {new: true})
+  })
   // Redirect to Homepage
   .then(() => res.redirect("/"))
+  .catch(err => console.log(`Err while creating the post in the DB: ${err}`));
 })
 
 module.exports = router;
