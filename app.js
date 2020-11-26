6dc4849419bc17e80bf5507fbf8a815361631a52
Laurence Rega <regalaurence@gmail.com>
2020-11-26 18:36:56 +0100

    API requirement added to app and routes created for search listennotes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 let result 
 unirest.get('https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0')
   .header('X-ListenAPI-Key', '92deae50310140ab877e8f1d4e4c8fcd').then(response=>{
-    console.log("response from api",response.toJSON())
+    //console.log("response from api",response.toJSON())
     result = response.toJSON()
   })
 //response.toJSON();
@@ -89,8 +89,8 @@ app.use('/', index);
 const authRouter = require('./routes/auth');
 app.use('/', authRouter);
 
-
-
+const listenNotes = require("./routes/listenNotes");
+app.use("/", listenNotes);
 
 const spotify = require("./routes/spotify");
 app.use("/spotify", spotify);
