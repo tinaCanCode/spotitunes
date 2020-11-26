9bfa2d342721c8380ae0f24af0f3b77cb533f092
Laurence Rega <regalaurence@gmail.com>
2020-11-26 20:15:11 +0100

    id of the podcast is retrievd when clicking on the button add to favorite

--- routes/listenNotes.js
+++ routes/listenNotes.js
@@ -6,7 +6,7 @@ const unirest = require('unirest');
 let result
 unirest.get('https://listen-api.listennotes.com/api/v2/search?q=star%20wars&type=podcast')
   .header('X-ListenAPI-Key', '92deae50310140ab877e8f1d4e4c8fcd').then(response => {
-    console.log("response from api",response.toJSON())
+    //console.log("response from api",response.toJSON())
     result = response.toJSON()
   })
 
@@ -16,14 +16,27 @@ router.get('/listennotes', (req, res, next) => {
 });
 
 router.get('/listennotes/search-results', (req, res) => {
-  console.log("HERE IS THE QUERY: " + req.query.podcast)
+  //console.log("HERE IS THE QUERY: " + req.query.podcast)
   unirest.get(`https://listen-api.listennotes.com/api/v2/search?q=${req.query.podcast}&type=podcast`)
     .header('X-ListenAPI-Key', '92deae50310140ab877e8f1d4e4c8fcd')
   .then((response) => {
-    console.log("the response: " + response.toJSON().body.results)
+    //console.log("the response: " + response.toJSON().body.results)
     response.toJSON()
     res.render('listenNotes/search-results', {searchResults : response.toJSON().body.results})
   })
+})
+
+router.post('/listennotes/:id/addtofavorite', (req, res) => {
+  // create new object in database
+  // push this ID to user "favorites" array
+  console.log("THE PARAMS: " + req.params.id)
+  // unirest.get(`https://listen-api.listennotes.com/api/v2/podcasts/${req.params.id}`)
+  //   .header('X-ListenAPI-Key', '92deae50310140ab877e8f1d4e4c8fcd')
+  // .then((response) => {
+  //   //console.log("the response: " + response.toJSON().body.results)
+  //   response.toJSON()
+  //   res.render('listenNotes/search-results', {searchResults : response.toJSON().body.results})
+  // })
 })
 
 module.exports = router;
