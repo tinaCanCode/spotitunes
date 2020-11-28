3e2d49d5fa96d74acfa73e69a0d10bab52ca2964
tinaCanCode <mc.scherzberg@googlemail.com>
2020-11-28 16:14:22 +0100

    add favorite and profile page for both apis working

--- models/Podcast.js
+++ models/Podcast.js
@@ -5,7 +5,7 @@ const podcastSchema = new Schema ({
     title: String,
     description: String,
     podcastId: {type: String, unique: true, require: true},
-    // Do we need to add the origin of the podcast? i.e. Spotify or ListenNotes?
+    origin: String
 })
 
 const Podcast = mongoose.model("Podcast", podcastSchema);
--- routes/listenNotes.js
+++ routes/listenNotes.js
@@ -5,23 +5,23 @@ const { exists } = require('../models/Podcast');
 const Podcast = require('../models/Podcast');
 const User = require('../models/User');
 
-/* GET search page */
-router.get('/listennotes', (req, res, next) => {
-  res.render('listenNotes/search');
-});
+// /* GET search page */
+// router.get('/listennotes', (req, res, next) => {
+//   res.render('listenNotes/search');
+// });
 
 
 
-router.get('/listennotes/search-results', (req, res) => {
-  //console.log("HERE IS THE QUERY: " + req.query.podcast)
-  unirest.get(`https://listen-api.listennotes.com/api/v2/search?q=${req.query.podcast}&type=podcast`)
-    .header('X-ListenAPI-Key', '92deae50310140ab877e8f1d4e4c8fcd')
-    .then((response) => {
-      //console.log("the response: " + response.toJSON().body.results)
-      response.toJSON()
-      res.render('listenNotes/search-results-ln', { searchResults: response.toJSON().body.results })
-    })
-})
+// router.get('/listennotes/search-results', (req, res) => {
+//   //console.log("HERE IS THE QUERY: " + req.query.podcast)
+//   unirest.get(`https://listen-api.listennotes.com/api/v2/search?q=${req.query.podcast}&type=podcast`)
+//     .header('X-ListenAPI-Key', '92deae50310140ab877e8f1d4e4c8fcd')
+//     .then((response) => {
+//       //console.log("the response: " + response.toJSON().body.results)
+//       response.toJSON()
+//       res.render('listenNotes/search-results-ln', { searchResults: response.toJSON().body.results })
+//     })
+// })
 
 // Listen Notes DETAILS page
 
@@ -39,6 +39,8 @@ router.get("/listennotes/details/:showId", (req, res) => {
 })
 
 
+// Add Listen Notes Podcasts as favorites
+
 router.post('/listennotes/:id/addtofavorite', (req, res) => {
   // create new object in database
   // push this ID to user "favorites" array
@@ -46,7 +48,7 @@ router.post('/listennotes/:id/addtofavorite', (req, res) => {
   Podcast.exists({ podcastId: req.params.id })
     .then(podcastExists => {
       if (!podcastExists) {
-        return Podcast.create({ podcastId: req.params.id })
+        return Podcast.create({ podcastId: req.params.id , origin: "listennotes"})
       } else {
         return Podcast.findOne({ podcastId: req.params.id })
       }
