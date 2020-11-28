e3812966f26f84b884a29d726d85f83d78ccf284
tinaCanCode <mc.scherzberg@googlemail.com>
2020-11-28 15:09:26 +0100

    details pages working

--- models/Podcast.js
+++ models/Podcast.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const podcastSchema = new Schema ({
     title: String,
     description: String,
-    podcastId: {type: String, unique: true, require: true}
+    podcastId: {type: String, unique: true, require: true},
     // Do we need to add the origin of the podcast? i.e. Spotify or ListenNotes?
 })
 
